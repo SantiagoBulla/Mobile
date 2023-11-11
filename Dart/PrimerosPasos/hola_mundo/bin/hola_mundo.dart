01ecefca20d219fcf20ddf51a27177a8cbd12255
Santiago Bulla Perez <santiagobulla1216@gmail.com>
2023-11-11 18:19:17 -0500

    crear primer proyecto Dart

---
+++ Dart/PrimerosPasos/hola_mundo/bin/hola_mundo.dart
@@ -0,0 +1,7 @@
+// import 'package:hola_mundo/hola_mundo.dart' as hola_mundo;
+
+void main(List<String> arguments) {
+  // print('Hello world: ${hola_mundo.calculate()}!');
+  print('Hola mundo');
+  print('Mi nombre es Santiago');
+}
