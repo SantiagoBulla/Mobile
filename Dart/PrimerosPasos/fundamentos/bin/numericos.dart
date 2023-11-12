cd31d225768a4ee76539d3f66660163746c77025
Santiago Bulla Perez <santiagobulla1216@gmail.com>
2023-11-12 11:14:50 -0500

    modulo 1 {variables, tipo de datos, concatenacion}

---
+++ Dart/PrimerosPasos/fundamentos/bin/numericos.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  double numero = 3;
+  double entero = 98;//agrega el .0 para que quede entero
+  print(numero);
+  int number = numero.toInt();
+  print(number);
+
+  //BOOLEAN
+  // bool verdadero = true;
+  // bool falso = false;
+  
+  print('el resultado de la suma es: ${entero + numero}');
+}
---
+++ Dart/PrimerosPasos/fundamentos/bin/strings.dart
@@ -0,0 +1,31 @@
+void main(List<String> args) {
+  //
+  String name = 'Jose';
+  final nombre = "David";
+  const texto = ''' 
+  con salto
+  de lineas
+  ''';
+
+  //METODOS
+  //--> parseo
+  int num = 3;
+  double decimal = 63.819111111;
+  String tres = num.toString();
+  String strDecimal = decimal.toStringAsFixed(5);
+  
+  String refran = ''' ya veremos dijo el ciego ''';
+  print(refran.contains('ciegos'));//evalua si la variable contiene esa palabra 
+  String email = 'santiagobulla1216@gmail.com';
+  print(email.split('@'));//divide la cadena de caracteres tomando como medio el @
+  print(email.split('@')[1]);//divide la cadena de caracteres tomando como medio el @ y devuelve la posicion 1 de la lista
+
+//evaula si empieza-termina con un caracter 
+  print(email.startsWith('s'));
+  print(email.endsWith('s'));
+
+  //posicion de un caracter dentro de un str
+  print(email.indexOf('a'));
+  //extrae una subcadena de un str
+  print(email.substring(email.indexOf('@')));//(inicio, final),(inicio)
+}
