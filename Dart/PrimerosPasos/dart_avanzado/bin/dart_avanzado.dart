ae17fc49d6789293bf7a03b62be81ddd67430303
Santiago Bulla Perez <santiagobulla1216@gmail.com>
2023-12-11 18:34:50 -0500

    crear proyecto dart avanzado

---
+++ Dart/PrimerosPasos/dart_avanzado/bin/dart_avanzado.dart
@@ -0,0 +1,5 @@
+import 'package:dart_avanzado/dart_avanzado.dart' as dart_avanzado;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_avanzado.calculate()}!');
+}
---
+++ Dart/PrimerosPasos/dart_avanzado/test/dart_avanzado_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_avanzado/dart_avanzado.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
